889cc4179c7f821b8770db7e58238b6fdb1f3bbc
Alberto Vazquez <apple@MacBook-Air.local>
2024-02-11 09:57:28 -0600

    adding a new component to my repository project

---
+++ frontend/src/auth/login.jsx
@@ -0,0 +1,9 @@
+
+
+export const Login = () => {
+  return (
+    <>
+        <div>Este es mi pnche login hijos de su puta madre</div>
+    </>
+  )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Login } from './auth/login'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
 )
