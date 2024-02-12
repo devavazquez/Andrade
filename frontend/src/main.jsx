375f57725893d1ff084428c8066e5507d6b2dac2
Alberto Vazquez <apple@MacBook-Air.local>
2024-02-11 21:56:46 -0600

    creating login UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Login } from './auth/login'
+import './auth/login.css'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
